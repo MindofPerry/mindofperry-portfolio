78428e0d0c7c8209df903a4bdd2d78ef59dd721b
Shane <96085056+MindofPerry@users.noreply.github.com>
2024-06-16 19:11:49 -0700

    Optimizations

--- script.js
+++ script.js
@@ -1,76 +1,79 @@
-// --- Smooth Scroll ---
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener('DOMContentLoaded', () => {
 
-        let headerHeight = document.querySelector('header').offsetHeight;
+    // --- Smooth Scroll ---
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-        let section = document.querySelector(this.getAttribute('href'));
-        let positionToScroll = section.offsetTop - headerHeight;
+            let headerHeight = document.querySelector('header').offsetHeight;
 
-        window.scrollTo({
-            top: positionToScroll,
-            behavior: 'smooth'
+            let section = document.querySelector(this.getAttribute('href'));
+            let positionToScroll = section.offsetTop - headerHeight;
+
+            window.scrollTo({
+                top: positionToScroll,
+                behavior: 'smooth'
+            });
         });
     });
-});
 
-// --- Dark mode toggle ---
-const darkModeToggle = document.getElementById('darkModeToggle');
-darkModeToggle.addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
+    // --- Dark mode toggle ---
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    darkModeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
 
-    document.querySelectorAll('.view-repo-button, .testimonial-button, input[type="submit"]').forEach(button => {
-        button.classList.toggle('dark-mode-text');
+        // Dark Mode Button Text
+        document.querySelectorAll('.view-repo-button, .testimonial-button, input[type="submit"]').forEach(button => {
+            button.classList.toggle('dark-mode-text');
+    
+        });
     });
-});
 
-// --- Loader ---
-window.addEventListener("load", function() {
-    const loader = document.getElementById('loader');
-    setTimeout(() => {
-        loader.style.opacity = "0";
+    // --- Loading Screen ---
+    window.addEventListener("load", function() {
+        const loader = document.getElementById('loader');
         setTimeout(() => {
-            loader.style.display = "none";
-        }, 500); // Half second after fading, hide the loader
-    }, 500); // Show the loader for at least half a second
-});
-
-// --- Back to Top Button ---
-const backToTopButton = document.getElementById('backToTop');
-
-window.addEventListener("scroll", function() {
-    if (window.scrollY > 300) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
-});
-
-backToTopButton.addEventListener("click", function() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+            loader.style.opacity = "0";
+            setTimeout(() => {
+                loader.style.display = "none";
+            }, 500); // Half second after fading, hide the loader
+        }, 500); // Show the loader for at least half a second
     });
-});
 
-// --- Hamburger Menu ---
-const hamburger = document.querySelector('.hamburger');
-const nav = document.querySelector('nav');
+    // --- Back to Top Button ---
+    const backToTopButton = document.getElementById('backToTop');
 
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
-
-const navLinks = document.querySelectorAll("nav a");
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        nav.classList.remove("active");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
     });
-});
 
-// --- Auto-Scroller For Testimonials & Recommendations ---
-document.addEventListener('DOMContentLoaded', function() {
+    backToTopButton.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+    // --- Hamburger Menu ---
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            nav.classList.remove("active");
+        });
+    });
+
+    // --- Auto-Scroller For Testimonials & Recommendations ---
     const container = document.querySelector('.testimonial-container');
     const testimonials = document.querySelectorAll('.testimonial');
     const testimonialCount = testimonials.length;
@@ -100,4 +103,5 @@ document.addEventListener('DOMContentLoaded', function() {
     // Stop scrolling when user hovers over the testimonials
     container.addEventListener('mouseenter', () => clearInterval(scrollInterval));
     container.addEventListener('mouseleave', () => scrollInterval = setInterval(scrollTestimonials, 5000));
+    
 });
